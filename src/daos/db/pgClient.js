2d0636aec5ec77f04e81a0f9dd33a601f0e82830
Martin Cueva <cuevabros@hotmail.com>
2026-05-27 18:31:32 -0300

    27-05-2026

--- src/daos/db/pgClient.js
+++ src/daos/db/pgClient.js
@@ -2,21 +2,17 @@
 import pkg from 'pg'
 const { Pool } = pkg;
 //import { Pool } from 'pg';
+import { HOST, DB_NAME, USER, PASSWORD, PORT } from './config.js';
 
  export const pool = new Pool({
-    user: 'postgres',
-    //host: '31.220.31.59',
-    host: '76.13.167.174',
-    //host: 'localhost',
-    database: 'gestion_escuelas',
-    //database: 'quinta',
-    //password: 'toba123*-a_postgres',
-    password: 'Sistema1y2+2',
-    //password: 'postgres',
-    //port: 5432
-    port: 5433
+    user: USER,
+    host: HOST,
+    database: DB_NAME,
+    password: PASSWORD,
+    port: PORT
   })
 
+  console.log(pool)
   //await pool.connect()
 
   //export const pgDb = pool.database
